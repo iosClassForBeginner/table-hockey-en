ad2cbae07d2e68c0e3980d77e8b8a145469e353d
Sam Kheirandish <sam.kheirandish@fortify.pro>
2017-09-14 09:55:35 -0700

    Improved the game
    More like a table hockey now

--- table hockey/GameScene.swift
+++ table hockey/GameScene.swift
@@ -9,16 +9,19 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // Define SpriteNodes
     var puck = SKSpriteNode()
     var playerBottom = SKSpriteNode()
     var playerTop = SKSpriteNode()
+    var bottomNet = SKSpriteNode()
+    var topNet = SKSpriteNode()
     
     // Define Labels
     var topLable = SKLabelNode()
     var bottomLable = SKLabelNode()
+    var pucksPhysics = SKPhysicsBody()
     
     var score = [Int]()
     
@@ -30,13 +33,16 @@ class GameScene: SKScene {
         puck = self.childNode(withName: "puck") as! SKSpriteNode
         playerBottom = self.childNode(withName: "playerBottom") as! SKSpriteNode
         playerTop = self.childNode(withName: "playerTop") as! SKSpriteNode
+        bottomNet = self.childNode(withName: "bottomNet") as! SKSpriteNode
+        topNet = self.childNode(withName: "topNet") as! SKSpriteNode
         
         // Link up Labels
         topLable = self.childNode(withName: "topLable") as! SKLabelNode
         bottomLable = self.childNode(withName: "bottomLable") as! SKLabelNode
         
         // Add directions to the puck as an impulse in movement
-        puck.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        pucksPhysics = puck.physicsBody!
+        puck.physicsBody?.applyImpulse(CGVector(dx: 0, dy: -20))
         
         // Define the game bounderies
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
@@ -45,23 +51,26 @@ class GameScene: SKScene {
         
         // Apply the boundaries
         self.physicsBody = border
+        self.physicsWorld.contactDelegate = self
     }
 
     func addScore(playerWhoWon: SKSpriteNode) {
         
         // Reset the puck position and zero its velocity
+        puck.physicsBody = nil
         puck.position = CGPoint(x: 0, y: 0)
-        puck.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        puck.physicsBody = pucksPhysics
+        
         
         // Add the score for the right player and push the ball in opponents direction
         if playerWhoWon == playerBottom {
             score[0] += 1
-            puck.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+            puck.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 20))
         } else if playerWhoWon == playerTop {
             score[1] += 1
-            puck.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+            puck.physicsBody?.applyImpulse(CGVector(dx: 0, dy: -20))
         }
-        
+
         // Display the score
         topLable.text = "\(score[1])"
         bottomLable.text = "\(score[0])"
@@ -84,13 +93,25 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         
-        playerTop.run(SKAction.moveTo(x: puck.position.x, duration: 0.2))
+        playerTop.run(SKAction.moveTo(x: puck.position.x, duration: 1))
         
-        if puck.position.y <= playerBottom.position.y - 70 {
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+        
+        if(firstBody.categoryBitMask == 2 && secondBody.categoryBitMask == 3 ||
+            firstBody.categoryBitMask == 3 && secondBody.categoryBitMask == 2)
+        {
             addScore(playerWhoWon: playerTop)
-        }
-        else if puck.position.y >= playerTop.position.y + 70 {
+           
+        } else if(firstBody.categoryBitMask == 2 && secondBody.categoryBitMask == 4 ||
+            firstBody.categoryBitMask == 4 && secondBody.categoryBitMask == 2)
+        {
             addScore(playerWhoWon: playerBottom)
         }
     }
+    
 }
